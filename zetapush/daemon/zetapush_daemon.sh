5fe045932df8f5eca04d23d02183ee9701012903
Javier <jaatadia@gmail.com>
2016-06-17 17:07:37 +0200

    Reconfigure to a daemon to make it work instead of the scheduler service

---
+++ zetapush/daemon/zetapush_daemon.sh
@@ -0,0 +1,32 @@
+if [ $# -eq 0 ];  then
+	echo "No arguments supplied"
+	echo "Usage: ./zetapush_daemon.sh configuration_file "
+fi
+
+source $1
+echo $user
+echo $pass
+echo $resource
+echo $businessID
+echo $authenticationID
+echo $macroID
+echo $macroName
+echo $conectedObject
+echo $loop
+
+while true; do
+
+	content=$(wget $conectedObject -q -t 1 --timeout=30 -O -)
+	echo "$content"
+
+	if [ "$content" != "" ]; then
+ 	
+		wget --header='Content-Type: application/json' --header='X-Authorization: {"data":{"password":"'$pass'", "login":"'$pass'"}, "resource":"'$resource'", "type":"'$businessID'.'$authenticationID'.simple"}' --post-data '{"name":"'$macroName'", "parameters":{"data":"'$content'"}}' 'http://free-1.zpush.io/str/restd/'$businessID'/'$macroID'/call' -qO- &> /dev/null
+	else
+		echo "could not connect with the object"
+	fi
+	
+	sleep $loop
+done
+
+
